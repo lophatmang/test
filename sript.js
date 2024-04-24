29259b315a51b6162851e293d21eb08969e1e37d
LoPhatMang <thanhntfx32690@funix.edu.vn>
2024-04-25 01:05:12 +0700

    a

--- sript.js
+++ sript.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const func = function () {
+  swal({
+    title: 'Trả lời câu hỏi',
+    text: 'Manh có phải đàn ông không',
+    icon: 'warning',
+    buttons: ['Không', 'Có'],
+  }).then(yes => {
+    if (yes) {
+      swal(
+        'Bạn trả lời rất đúng',
+        'Manh là đàng đàn ống chính hiệu',
+        'success'
+      );
+    } else {
+      swal('Bạn trả lời sai', 'hãy trả lời lại đi', 'error').then(yes => {
+        if (yes) return func();
+      });
+    }
+  });
+};
+
+func();
